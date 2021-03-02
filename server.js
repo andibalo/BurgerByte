a276a784667060364ff7529577edc067a1eefc50
andi balo <andibalo213@gmail.com>
2021-03-02 08:27:16 +0700

    message

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const connectDb = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
     });
   } catch (error) {
     console.log(error);
